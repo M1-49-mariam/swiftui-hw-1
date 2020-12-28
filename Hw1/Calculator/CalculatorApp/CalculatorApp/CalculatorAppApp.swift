b0fb99225ab86100800048b88af8dc18180c96da
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2020-12-28 18:42:42 +0300

    Create CalculatorAppApp.swift

---
+++ Hw1/Calculator/CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Mariam2 on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
